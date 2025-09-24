dcb7e631dac09a9c43d73f2ca47bbabe43b70cf7
vidhatrihr <167363972+vidhatrihr@users.noreply.github.com>
2025-09-24 09:46:58 +0530

    api in image search app

--- image-search-app/main.js
+++ image-search-app/main.js
@@ -1,13 +1,29 @@
 async function getPhotos() {
-  const accesskey = 'EJkN_ZCtAARx7ENuEbRJ1TaGUED_YcsMCStbCStMTX0';
-  const response = await fetch('https://api.unsplash.com/photos/random?count=1', {
+  const accessKey = 'EJkN_ZCtAARx7ENuEbRJ1TaGUED_YcsMCStbCStMTX0';
+  const query = document.querySelector('input').value;
+  const response = await fetch(`https://api.unsplash.com/search/photos?query=${query}`, {
     headers: {
-      Authorization: `Client-ID ${accesskey}`,
+      Authorization: `Client-ID ${accessKey}`,
     },
   });
 
+  // const response = await fetch('data.json');
+  document.querySelector('.image-container').innerHTML = '';
+
   const data = await response.json();
-  console.log(data);
+  data.results.forEach(element => {
+    const caption = element.alt_description[0].toUpperCase() + element.alt_description.slice(1);
+    const html = `
+        <div class="image-result">
+          <img src="${element.urls.regular}" alt="${caption}" />
+          <p>${caption}</p>
+        </div>
+    `;
+    document.querySelector('.image-container').innerHTML += html;
+  });
 }
 
-getPhotos();
+document.querySelector('.search-container').addEventListener('submit', event => {
+  event.preventDefault();
+  getPhotos();
+});
