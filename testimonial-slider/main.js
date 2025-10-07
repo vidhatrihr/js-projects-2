69f8a9ea8ac038ff5a49afbb36b6379a71a35f68
vidhatrihr <167363972+vidhatrihr@users.noreply.github.com>
2025-10-07 17:32:29 +0530

    timer added

--- testimonial-slider/main.js
+++ testimonial-slider/main.js
@@ -1,38 +1,45 @@
+const html = String.raw;
+
 let testimonials = [];
+let timeoutRef = null;
 
 let nameEl = document.querySelector('.name');
 let profileEl = document.querySelector('.profile');
 let textEl = document.querySelector('.text');
 
-let prevIdx = 0;
+let activeIdx = parseInt(new URLSearchParams(location.search).get('idx') ?? 0);
+let timeoutDuration = parseFloat(new URLSearchParams(location.search).get('timeout') ?? 5) * 1000;
+
 function changeTestimonial(idx) {
+  console.log(`Change testimonial at ${new Date().getSeconds()}`);
+
   nameEl.innerText = testimonials[idx].name;
   profileEl.src = testimonials[idx].url;
   textEl.innerText = testimonials[idx].testimonial;
 
-  document.querySelector(`.btn-${prevIdx}`).classList.remove('active');
+  document.querySelector(`.btn-${activeIdx}`).classList.remove('active');
   document.querySelector(`.btn-${idx}`).classList.add('active');
-  prevIdx = idx;
+
+  activeIdx = idx;
+
+  // trigger timeout that does changeTestimonial(idx + 1), after 2 sec
+  clearTimeout(timeoutRef);
+
+  timeoutRef = setTimeout(() => {
+    changeTestimonial((idx + 1) % testimonials.length);
+  }, timeoutDuration);
 }
 
 function generateButtons() {
   testimonials.forEach((elem, idx) => {
-    let activeClass = idx == prevIdx ? 'active' : '';
-    let html = `
-          <button class="btn-${idx} ${activeClass}" onclick="changeTestimonial(${idx})"></button>    
+    let activeClass = idx == activeIdx ? 'active' : '';
+    let buttonHtml = html`
+      <button class="btn-${idx} ${activeClass}" onclick="changeTestimonial(${idx})"></button>
     `;
 
-    document.querySelector('.controls').innerHTML += html;
+    document.querySelector('.controls').innerHTML += buttonHtml;
   });
 }
-
-// fetch('https://gist.githubusercontent.com/k26rahul/faaa120cf8f254232ca7baf69920f57b/raw')
-//   .then(response => {
-//     return response.json();
-//   })
-//   .then(data => {
-//     console.log(data);
-//   });
 
 async function getData() {
   let response = await fetch(
@@ -41,7 +48,8 @@ async function getData() {
   testimonials = await response.json();
 
   generateButtons();
-  changeTestimonial(0);
+  changeTestimonial(activeIdx);
+
   document.querySelector('.container').style.visibility = 'visible';
 }
 
