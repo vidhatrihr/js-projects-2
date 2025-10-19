ae4ea1c3a15cb7d648e35313ef21e519298deb15
vidhatrihr <167363972+vidhatrihr@users.noreply.github.com>
2025-10-19 17:53:22 +0530

    loan calc done

--- loan-calc/main.js
+++ loan-calc/main.js
@@ -1,34 +1,80 @@
 const html = String.raw;
 
-let principalInput = document.querySelector('#principal');
-let interestInput = document.querySelector('#interest');
-let fixedPaymentInput = document.querySelector('#fixed-payment');
-let checkboxInput = document.querySelector('#checkbox');
+const principalInput = document.querySelector('#principal');
+const interestInput = document.querySelector('#interest');
+const fixedPaymentInput = document.querySelector('#fixed-payment');
+const DurationSelect = document.querySelector('#duration-select');
+const checkboxInput = document.querySelector('#checkbox');
 
-let summaryLastMonthEl = document.querySelector('.summary-last-month');
-let summaryMonthsEl = document.querySelector('.summary-months');
-let summaryPrincipalEl = document.querySelector('.summary-principal');
-let summaryInterestEl = document.querySelector('.summary-interest');
-let summaryPaymentEl = document.querySelector('.summary-payment');
+const errorMessageEl = document.querySelector('#error-message');
+const summaryLastMonthEl = document.querySelector('.summary-last-month');
+const summaryMonthsEl = document.querySelector('.summary-months');
+const summaryMonthlyPaymentEl = document.querySelector('.summary-monthly-payment');
+const summaryPrincipalEl = document.querySelector('.summary-principal');
+const summaryInterestEl = document.querySelector('.summary-interest');
+const summaryPaymentEl = document.querySelector('.summary-payment');
 
 principalInput.addEventListener('input', calculateLoan);
 interestInput.addEventListener('input', calculateLoan);
 fixedPaymentInput.addEventListener('input', calculateLoan);
+DurationSelect.addEventListener('change', calculateLoan);
 checkboxInput.addEventListener('change', calculateLoan);
+
+document.querySelectorAll('input[name=calculationMode]').forEach(radio => {
+  radio.addEventListener('change', () => {
+    let show = radio.value;
+    let hide = radio.value == 'fixedDuration' ? 'fixedPayment' : 'fixedDuration';
+
+    document.querySelector(`label[data-${show}]`).classList.remove('hidden');
+    document.querySelector(`label[data-${hide}]`).classList.add('hidden');
+
+    calculateLoan();
+  });
+});
+
+const rupeeFormatter = new Intl.NumberFormat('en-IN', {
+  style: 'currency',
+  currency: 'INR',
+  minimumFractionDigits: 2,
+});
 
 calculateLoan();
 
 function calculateLoan() {
-  console.log('calculateLoan');
-  document.querySelector('tbody').innerHTML = '';
+  resetUI();
 
-  const principal = parseFloat(principalInput.value);
-  const interestRate = parseFloat(interestInput.value) / 100 / 12;
-  const monthlyPayment = parseFloat(fixedPaymentInput.value);
+  let principal = parseFloat(principalInput.value);
+  let interestRate = parseFloat(interestInput.value) / 100 / 12;
+  let monthlyPayment = parseFloat(fixedPaymentInput.value);
+  let duration = parseFloat(DurationSelect.value);
+  let calculationMode = document.querySelector('input[name=calculationMode]:checked').value;
 
-  if (principal <= 0 || interestRate < 0 || monthlyPayment <= 0) {
-    console.warn('Invalid inputs');
+  if (!(principal > 0 && interestRate >= 0 && monthlyPayment > 0)) {
+    errorMessageEl.innerText = 'Invalid inputs';
+    errorMessageEl.hidden = false;
     return;
+  }
+
+  if (calculationMode == 'fixedDuration') {
+    if (interestRate == 0) {
+      monthlyPayment = principal / duration;
+    } else {
+      // monthlyPayment  = [ p x r x (1 + r)^n] / [(1 + r)^n -1]
+      let p = principal;
+      let r = interestRate;
+      let n = duration;
+
+      monthlyPayment = (p * r * (1 + r) ** n) / ((1 + r) ** n - 1);
+
+      if (Number.isNaN(monthlyPayment)) {
+        errorMessageEl.innerText = 'Calculation failed. Decrease duration or interest rate.';
+        errorMessageEl.hidden = false;
+      }
+
+      if (monthlyPayment - principal * interestRate < 0.01) {
+        monthlyPayment += 0.01;
+      }
+    }
   }
 
   let balance = principal;
@@ -39,35 +85,60 @@ function calculateLoan() {
     let principalPaid = monthlyPayment - interestPaid;
 
     if (principalPaid <= 0) {
-      console.warn('Loan can never be paid,', { principalPaid, interestPaid });
+      errorMessageEl.innerText =
+        'Monthly payment can never be paid off. Please increase the payment.';
+      errorMessageEl.hidden = false;
       return;
     }
 
     balance -= principalPaid;
-    monthsCount += 1;
+    balance = balance > 0 && balance < 0.01 ? 0 : balance;
 
-    console.log(interestPaid, principalPaid, balance);
+    monthsCount += 1;
 
     updateTable({ monthsCount, interestPaid, principalPaid, balance });
   }
+
+  if (calculationMode == 'fixedDuration' && monthsCount < duration) {
+    errorMessageEl.innerText = `Not possible to extend your EMI over ${duration} months.`;
+    errorMessageEl.hidden = false;
+  }
+
   updateSummary({ monthlyPayment, monthsCount, principal, balance });
+}
+
+function resetUI() {
+  errorMessageEl.hidden = true;
+  document.querySelector('tbody').innerHTML = '';
+
+  summaryLastMonthEl.innerText = '_';
+  summaryMonthsEl.innerText = '_';
+  summaryMonthlyPaymentEl.innerText = '_';
+  summaryPrincipalEl.innerText = '_';
+  summaryInterestEl.innerText = '_';
+  summaryPaymentEl.innerText = '_';
 }
 
 function updateTable({ monthsCount, interestPaid, principalPaid, balance }) {
   let date = new Date();
   date.setMonth(date.getMonth() + monthsCount - checkboxInput.checked);
 
-  date = date.toLocaleDateString(undefined, {
+  let formattedDate = date.toLocaleDateString(undefined, {
     year: 'numeric',
     month: 'short',
   });
 
+  if (balance <= 0) {
+    principalPaid += balance;
+    balance = 0;
+  }
+
   let template = html`
     <tr>
-      <td>${date}</td>
-      <td>₹19</td>
-      <td>₹1</td>
-      <td>₹80</td>
+      <td>${formattedDate}</td>
+      <td>${rupeeFormatter.format(principalPaid)}</td>
+      <td>${rupeeFormatter.format(interestPaid)}</td>
+      <td>${rupeeFormatter.format(balance)}</td>
     </tr>
   `;
 
@@ -78,21 +149,16 @@ function updateSummary({ monthlyPayment, monthsCount, principal, balance }) {
   let date = new Date();
   date.setMonth(date.getMonth() + monthsCount - checkboxInput.checked);
 
-  date = date.toLocaleDateString(undefined, {
+  let formattedDate = date.toLocaleDateString(undefined, {
     year: 'numeric',
     month: 'short',
   });
 
-  const rupeeFormatter = new Intl.NumberFormat('en-IN', {
-    style: 'currency',
-    currency: 'INR',
-    minimumFractionDigits: 2,
-  });
-
   let totalPayment = monthlyPayment * monthsCount + balance;
 
-  summaryLastMonthEl.innerText = date;
+  summaryLastMonthEl.innerText = formattedDate;
   summaryMonthsEl.innerText = monthsCount;
+  summaryMonthlyPaymentEl.innerText = rupeeFormatter.format(monthlyPayment);
   summaryPrincipalEl.innerText = rupeeFormatter.format(principal);
   summaryInterestEl.innerText = rupeeFormatter.format(totalPayment - principal);
   summaryPaymentEl.innerText = rupeeFormatter.format(totalPayment);
